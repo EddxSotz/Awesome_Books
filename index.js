bc6d1c8622730c41b8df17ad0dfae6e4f874f3f6
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-07-04 11:01:12 -0600

    set basic JS variables

---
+++ index.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form'); //Store html form element to variable
+const bookList = document.getElementById('bookList'); //store html ul element to var
+
+let booksList = JSON.parse(localStorage.getItem('books')) || []; //get local storage of books saved and pass it to object or array if false
+
