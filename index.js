cc6cef2f9810dc94c88e102a8677b983c82f707c
Javier Jesus Martínez Fariñas <86327867+jlcarrascof@users.noreply.github.com>
2023-07-04 13:07:50 -0400

    create addBook function

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const bookList = document.getElementById('bookList'); //store html ul element to
 
 let booksList = JSON.parse(localStorage.getItem('books')) || []; //get local storage of books saved and pass it to object or array if false
 
+function addBook(title, author) {
+  const newBook = { title, author };
+  booksList.push(newBook);
+  localStorage.setItem('books', JSON.stringify(booksList));
+  displayBooks();
+}
