0d17d9e21f802e2d1a0efe1c79b41db628883400
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-07-05 21:51:35 -0600

    create Book class

--- index.js
+++ index.js
@@ -3,42 +3,10 @@ const bookList = document.getElementById('bookList'); // store html ul element t
 
 let booksList = JSON.parse(localStorage.getItem('books')) || []; // get local storage of books saved and pass it to object or array if false
 
-function addBook(title, author) { // Gets parameter, pushes to booksList and saves to local storage
-  const newBook = { title, author };
-  booksList.push(newBook);
-  localStorage.setItem('books', JSON.stringify(booksList));
-  /* eslint-disable no-use-before-define */
-  displayBooks(); // Call display books to update list
+class Book {
+  constructor() {
+    this.bookTitle = document.getElementById('titleInput');
+    this.bookAuthor = document.getElementById('authorInput');
+  }
+
 }
-
-function removeBook(book) {
-  booksList = booksList.filter((a) => a !== book); // make filtered array and save to local storage
-  localStorage.setItem('books', JSON.stringify(booksList));
-  displayBooks();
-}
-
-function displayBooks() {
-  bookList.innerHTML = ''; // clear out list
-  booksList.forEach((book) => { // loop through each book array element
-    const li = document.createElement('li'); // create a list item each loop
-    li.innerHTML = `${book.title} <br> ${book.author} <br>`; // insert the content to li item
-    const removeBtn = document.createElement('button'); // create remove btn
-    const hr = document.createElement('hr');
-    removeBtn.innerHTML = 'Remove';
-    removeBtn.addEventListener('click', () => removeBook(book)); // when remove btn clicked call removeBook function and pass parameter
-    li.appendChild(removeBtn); // append remove btn
-    bookList.appendChild(li); // append li item
-    bookList.appendChild(hr);
-  });
-}
-
-displayBooks();
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const bookTitle = document.getElementById('titleInput');
-  const bookAuthor = document.getElementById('authorInput');
-  addBook(bookTitle.value, bookAuthor.value);
-  bookTitle.value = '';
-  bookAuthor.value = '';
-});
